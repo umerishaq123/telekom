5e9ad9cc49c440c0633e4affeea459b2fcc8ab96
logoutbye <muhammadbilal03478@gmail.com>
2024-07-12 00:17:32 +0500

    loa

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,125 @@
+// import 'package:firebase_core/firebase_core.dart';
+// import 'package:flutter/material.dart';
+// import 'package:get/get.dart';
+// import 'package:get/get_connect/http/src/utils/utils.dart';
+// import 'package:provider/provider.dart';
+// import 'package:telekom2/screens/authentication/LoginScreeen.dart';
+// import 'package:telekom2/screens/chats/firebase_provider.dart';
+// // Import the generated file
+// import 'firebase_options.dart';
+
+// void main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   await Firebase.initializeApp(
+//     options: DefaultFirebaseOptions.currentPlatform,
+//   );
+//   runApp(const MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     return MultiProvider(
+//       providers: [
+//         ChangeNotifierProvider(create: (context) => FirebaseProvider()),
+//       ],
+//       child: const GetMaterialApp(
+//         debugShowCheckedModeBanner: false,
+//         home: LoginScreen(),
+//       ),
+//     );
+//   }
+// }
+
+// //// code is hare
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+// import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:telekom2/screens/authentication/LoginScreeen.dart';
+import 'package:provider/provider.dart';
+import 'package:telekom2/screens/chats/firebase_provider.dart';
+import 'package:telekom2/screens/new_chat_module/provider/firebase_provider.dart';
+import 'package:telekom2/screens/new_chat_module/view/screens/auth/verify_email_page.dart';
+
+import 'firebase_options.dart';
+import 'screens/new_chat_module/view/screens/auth/auth_page.dart';
 
 
-void main() {
+// Future<void> _backgroundMessageHandler(
+//     RemoteMessage message) async {
+//   await Firebase.initializeApp(
+//     options: DefaultFirebaseOptions.currentPlatform,
+//   );
+// }
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  // await FirebaseMessaging.instance.getInitialMessage();
+
+  // FirebaseMessaging.onBackgroundMessage(
+  //     _backgroundMessageHandler);
   runApp(const MyApp());
 }
+
+final navigatorKey = GlobalKey<NavigatorState>();
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return const GetMaterialApp(
+  get mainColor => null;
 
-      home:  LoginScreen(),
-    );
-  }
+  @override
+  Widget build(BuildContext context) =>
+      ChangeNotifierProvider(
+        create: (_) => FirebaseProvider(),
+        child: MaterialApp(
+          navigatorKey: navigatorKey,
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+              elevatedButtonTheme: ElevatedButtonThemeData(
+                style: ElevatedButton.styleFrom(
+                    textStyle:
+                        const TextStyle(fontSize: 20),
+                    minimumSize: const Size.fromHeight(52),
+                    backgroundColor: mainColor),
+              ),
+              appBarTheme: const AppBarTheme(
+                backgroundColor: Colors.transparent,
+                elevation: 0,
+                titleTextStyle: TextStyle(
+                  color: Colors.black,
+                  fontSize: 35,
+                  fontWeight: FontWeight.bold,
+                ),
+              )),
+          home: const MainPage(),
+        ),
+      );
 }
 
-//// code is hare
+class MainPage extends StatelessWidget {
+  const MainPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        body: StreamBuilder<User?>(
+          stream: FirebaseAuth.instance.authStateChanges(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return const VerifyEmailPage();
+            } else {
+              return const AuthPage();
+            }
+          },
+        ),
+      );
+}
